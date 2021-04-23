434deafd8c1adc98fab50eead379b3f07c6ed2fb
Qoalabear <512290+qoala@users.noreply.github.com>
2021-04-22 23:16:18 -0700

    Update makeconfig to use workshop ID

--- makeconfig.example.mk
+++ makeconfig.example.mk
@@ -3,6 +3,6 @@
 # Copy this as makeconfig.mk and edit that file for the paths on your system.
 
 KWAD_BUILDER=~/kwad-builder/bin/linux/builder
-INSTALL_PATH=$(REPLACEME)/steamapps/common/InvisibleInc/mods/backstabprotocol
+INSTALL_PATH=$(REPLACEME)/steamapps/common/InvisibleInc/mods/workshop-2446230676
 # Uncomment the following line if you maintain separate "all mods" and "active mods" folders
 # INSTALL_PATH2=
